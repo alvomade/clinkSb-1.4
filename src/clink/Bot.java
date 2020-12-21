d3ec456c7bf580d1c8533bedba61074ef779cdd0
alvomade <malvinokeyo@gmail.com>
2020-12-22 01:48:26 +0300

    first commit for 1.4

---
+++ src/clink/Bot.java
@@ -0,0 +1,228 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clink;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author alvo
+ */
+public class Bot{
+    static String minPrice,maxPrice,avPrice; 
+    static boolean isLoggedIn, isClosed=false;
+   //count number of available orders
+   static int availableOrders(WebDriver driver){
+       
+        WebDriverWait waitForOrderCount=new WebDriverWait(driver,100);
+        WebElement foundOrders=waitForOrderCount.until(ExpectedConditions.visibilityOfElementLocated(By.className("irbJoc")));
+        String foundOrdersString=foundOrders.getText();
+        String[] foundArray=foundOrdersString.split(" ");
+        int numberOfOrders = Integer.parseInt(foundArray[1]);
+       return numberOfOrders;
+   } 
+   
+   public static boolean login(WebDriver driver,String email,String password){
+       //open the browser and log in
+    driver.get("https://studybay.app/");
+    
+    WebDriverWait waitForEmailField=new WebDriverWait(driver,10);
+    WebElement emailField=waitForEmailField.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
+    emailField.sendKeys(email);
+    
+    WebDriverWait waitForPassField=new WebDriverWait(driver,10);
+    WebElement passField=waitForPassField.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+    passField.sendKeys(password);
+    
+    driver.findElement(By.xpath("//button")).sendKeys(Keys.RETURN);
+
+    try{
+    WebDriverWait waitForLogin=new WebDriverWait(driver,10);
+    WebElement loginError=waitForLogin.until(ExpectedConditions.visibilityOfElementLocated(By.className("cx-login-form-error")));
+    }catch(Exception e){
+        isLoggedIn=true;
+        driver.get("https://studybay.app/order/search");
+    }
+    
+    return isLoggedIn;
+   }
+   
+   public static void bid(WebDriver driver,int delay,String priceLevel,String bidOn,String[] filter, javax.swing.JTextArea displayLog) throws InterruptedException, IOException{
+       int errorHeperer=0;
+        int i=0;     
+        WebDriverWait wait=new WebDriverWait(driver,15);
+        
+       //first time refresh
+        if(errorHeperer==0){
+           errorHeperer++;
+           driver.navigate().refresh();
+        }
+        //end of first time refresh
+        while(i>=0){
+        List<WebElement> orders=driver.findElements(By.xpath("//div[@class='orderA__order' or @class=' orderA__order--read' or @class='orderA__order--paid'  or @class='orderA__order--premium'] "));
+        
+        //if no new order,wait before refresh
+        if(orders.size()<=0){
+         driver.manage().timeouts().implicitlyWait(delay, TimeUnit.SECONDS);
+         orders=driver.findElements(By.xpath("//div[@class='orderA__order' or @class=' orderA__order--read' or @class='orderA__order--paid'  or @class='orderA__order--premium'] "));
+        }
+        
+        System.out.println("found "+orders.size()+" orders");
+        displayLog.append("found "+orders.size()+" order(s) \n");
+        displayLog.append("-----------------------------------------------------------------------------"+"\n");
+        
+        for(WebElement order:orders){
+                //click the more button
+                try{
+                    wait.until(ExpectedConditions.elementToBeClickable(order.findElement(By.className("expandButton__expand")))).click();
+                    System.out.println("expand btn clicked");
+                    displayLog.append("Bidding order... \n");
+                    
+                }catch(Exception e){
+                    e.printStackTrace();
+                    Config.errorFileWriter(e.toString());
+                    displayLog.append("failed to click the more button, fixing... \n");
+                    continue;
+                }
+              
+                 //check for unwanted orders
+                if(filter.length>0){
+                  String cart=order.findElement(By.className("orderA__category")).getText();
+                  String[] cartArray=cart.split(",");
+                  String subject;
+                  try{
+                      subject=cartArray[1];
+                  }catch(ArrayIndexOutOfBoundsException e){
+                      subject="nothing";
+                  }
+                  
+                  subject=subject.trim();
+                  List<String> list = Arrays.asList(filter);
+                  
+                  if(list.contains(subject)){
+                    order.findElement(By.className("expandButton__expand")).click();
+                    displayLog.append(subject+" is unwanted, looking for new orders... \n");
+                    continue;
+                  }
+                  
+              
+              }
+                System.out.println("its wanted");
+                displayLog.append("subject is accepted \n");
+                // end of check for unwanted orders
+
+                //click start bidding button
+                try{
+                  order.findElement(By.className("styled__MakeBidButton-sc-1p6owd5-9")).click();
+                }catch(Exception e){
+                    System.out.println(e);
+                    Config.errorFileWriter(e.toString());
+                    continue;
+                }
+                //end of click start bidding button  
+       
+                  //message placing
+                  try{
+                       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='styled__Field-sc-13g4w8f-1 ePCfsj'])[last()]"))).click();
+                     }catch(Exception e){
+                        displayLog.append("message placing failed,looking for a fix \n"); 
+                        driver.navigate().refresh();
+                        Config.errorFileWriter(e.toString());
+                        Thread.sleep(delay*1000);
+                        continue;
+                    }
+                    
+                        
+                        try{
+                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='styled__Wrapper-sc-4f1cbs-0 gTBVTO'])[last()]"))).click();
+                        System.out.println("message placed"); 
+                        displayLog.append("message placed \n");
+                        }catch(Exception e){
+                        System.out.println("message was not selected");  
+                        displayLog.append("message was not selected \n");
+                        driver.navigate().refresh();
+                        Config.errorFileWriter(e.toString());
+                        Thread.sleep(delay*1000);
+                        continue;
+                        }
+                    //end of message placing
+                    
+                    if(!priceLevel.equalsIgnoreCase("none")){   
+                    //extract prces ranges from modal
+                        minPrice=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[@class='styled__Amount-qyovge-7 cTVHAe'])[last()]"))).getText().substring(1);
+                        avPrice=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[@class='styled__Amount-qyovge-7 gQtpqC'])[last()]"))).getText().substring(1);
+                        maxPrice=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[@class='styled__Amount-qyovge-7 gSIIkh'])[last()]"))).getText().substring(1);
+                          
+              
+                        WebElement inputAmount=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[@class='styled__Wrapper-sc-1novt9v-2 fLTTlZ']/div/div/input)[last()]")));
+
+                            switch(priceLevel){
+                                case "Minimum":
+                                    inputAmount.sendKeys(minPrice);
+                                    displayLog.append("price set: "+"$ "+minPrice+"\n");
+                                    break;
+                                case "Average":
+                                    inputAmount.sendKeys(avPrice);
+                                    displayLog.append("price set: "+"$ "+avPrice+"\n");
+                                    break;
+                                case "Maximum":
+                                    inputAmount.sendKeys(maxPrice);
+                                    displayLog.append("price set: "+"$ "+maxPrice+"\n");
+                                    break;
+                                default:
+                                    //inputAmount.sendKeys(" ");
+                                    break;    
+                            }
+                            
+                    }
+//                     end of check price level
+                    
+                        try{
+                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='ui-modal-content']/div[@class='ui-modal-body']/div/div[@class='sb-makeOffer__actions']/div/button[1])[last()]"))).click();
+                        }
+                        catch(Exception e){
+                        driver.navigate().refresh();
+                        Config.errorFileWriter(e.toString());
+                        Thread.sleep(delay*1000);
+                        }
+                        displayLog.append("bidded on: "+order.findElement(By.className("orderA__name")).getText()+"\n");
+                        displayLog.append("-----------------------------------------------------------------------------"+"\n");
+                        System.out.println("Bidd title: "+order.findElement(By.className("orderA__name")).getText());
+                        Thread.sleep(delay*1000);
+                        
+               
+        }
+        driver.navigate().refresh();
+        
+        }
+        
+    
+   }
+   
+   public static void exitBrowser(WebDriver driver){
+       driver.close();
+   }
+   
+   
+}
---
+++ src/clink/clinkHome.java
@@ -0,0 +1,351 @@
+
+package clink;
+
+import org.apache.commons.lang3.text.WordUtils;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Collections;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class clinkHome extends javax.swing.JFrame {
+    
+    static String email, code, expiry, password=null;
+    String bidStatus = "null";
+    boolean suspended = false;
+    WebDriver driver = null;
+    private int x;
+    private int y;
+    
+    Thread bidding = new Thread(new Runnable() {
+        public void run() {
+            try {
+                 clink();              
+            }
+            catch(Exception t) {
+                 // appropriate error reporting here
+            } 
+        }
+    });
+    
+    public clinkHome(String email, String code, String expiry, String password) {
+        super("Clink-home/"+email);
+        this.email=email;
+        this.code=code;
+        this.expiry=expiry;
+        this.password=password;
+        
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                int result = JOptionPane.showConfirmDialog(null, "Do you want to Exit ?", "Exit Confirmation : ",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                } else if (result == JOptionPane.NO_OPTION) {
+                    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                }
+            }
+        });
+        setUndecorated(true);
+        pack();
+        java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int height = screenSize.height;
+        int width = screenSize.width;
+        setSize(width / 3, height / 2);
+
+        // here's the part where i center the jframe on screen
+        setLocationRelativeTo(null);
+        
+        setVisible(true);
+        initComponents();
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        home_btnLaunch = new javax.swing.JButton();
+        home_btnExit = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        displayLog = new javax.swing.JTextArea();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(45, 45, 45));
+        setForeground(new java.awt.Color(45, 45, 45));
+        setResizable(false);
+        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                formMouseDragged(evt);
+            }
+        });
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                formMousePressed(evt);
+            }
+        });
+
+        jPanel1.setBackground(new java.awt.Color(19, 28, 59));
+        jPanel1.setForeground(new java.awt.Color(19, 28, 59));
+
+        home_btnLaunch.setBackground(new java.awt.Color(0, 204, 0));
+        home_btnLaunch.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        home_btnLaunch.setForeground(new java.awt.Color(255, 255, 255));
+        home_btnLaunch.setText("START");
+        home_btnLaunch.setBorderPainted(false);
+        home_btnLaunch.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                home_btnLaunchMousePressed(evt);
+            }
+        });
+        home_btnLaunch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                home_btnLaunchActionPerformed(evt);
+            }
+        });
+
+        home_btnExit.setBackground(new java.awt.Color(28, 39, 84));
+        home_btnExit.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        home_btnExit.setForeground(new java.awt.Color(255, 255, 255));
+        home_btnExit.setText("EXIT");
+        home_btnExit.setBorderPainted(false);
+        home_btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                home_btnExitActionPerformed(evt);
+            }
+        });
+
+        jScrollPane2.setBackground(new java.awt.Color(0, 0, 0));
+        jScrollPane2.setForeground(new java.awt.Color(255, 255, 255));
+
+        displayLog.setEditable(false);
+        displayLog.setBackground(new java.awt.Color(19, 28, 59));
+        displayLog.setColumns(20);
+        displayLog.setForeground(new java.awt.Color(255, 255, 255));
+        displayLog.setRows(5);
+        displayLog.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(28, 0, 213)));
+        jScrollPane2.setViewportView(displayLog);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(58, 58, 58)
+                .addComponent(home_btnLaunch, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 122, Short.MAX_VALUE)
+                .addComponent(home_btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(79, 79, 79))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(home_btnLaunch, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(home_btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(21, 21, 21))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void home_btnLaunchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_home_btnLaunchActionPerformed
+       
+        if(bidStatus == "null"){
+            bidStatus = "started";
+            bidding.start();     
+         }else if(bidStatus == "stopped"){
+            bidStatus = "started";
+            Thread bidding = new Thread(new Runnable() {
+            public void run() {
+            try {
+                 clink();              
+            }
+            catch(Exception t) {
+                 // appropriate error reporting here
+            } 
+                }
+             });
+            bidding.start();
+            
+         }else if(bidStatus == "started"){
+            bidStatus = "stopped";
+            bidding.stop();
+            driver.close(); //closes the browser
+            
+         }
+        
+        
+    }//GEN-LAST:event_home_btnLaunchActionPerformed
+
+    private void home_btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_home_btnExitActionPerformed
+        int result = JOptionPane.showConfirmDialog(null, "Do you want to Exit ?", "Exit Confirmation : ",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    this.dispose();
+                } else if (result == JOptionPane.NO_OPTION) {
+                    setDefaultCloseOperation(clinkHome.DO_NOTHING_ON_CLOSE);
+                }
+    }//GEN-LAST:event_home_btnExitActionPerformed
+
+    private void home_btnLaunchMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_home_btnLaunchMousePressed
+        if(bidStatus == "null" || bidStatus == "stopped"){
+            home_btnLaunch.setBackground(Color.red);
+            home_btnLaunch.setText("STOP");
+            displayLog.append("Happy Bidday\n");
+            displayLog.append("Firing up clinkbot...\n");
+        
+        }else if(bidStatus == "started"){
+            home_btnLaunch.setBackground(Color.green);
+            home_btnLaunch.setText("START");
+            displayLog.append("Stopping Bid...\n");
+            displayLog.append("Bid stopped...\n");
+            return;
+        }
+        
+    }//GEN-LAST:event_home_btnLaunchMousePressed
+
+    private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
+        int x = evt.getXOnScreen()-this.x;
+        int y = evt.getYOnScreen()-this.y;
+        this.setLocation(x,y); 
+    }//GEN-LAST:event_formMouseDragged
+
+    private void formMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMousePressed
+        x = evt.getX();
+        y = evt.getY();
+    }//GEN-LAST:event_formMousePressed
+
+    protected void clink() throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver", "drv.exe");
+//        System.setProperty("webdriver.chrome.driver", "drv.exe");
+
+    //removes chrome default test notification
+    ChromeOptions options = new ChromeOptions();
+    options.setExperimentalOption("useAutomationExtension", false);
+    options.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation"));   
+//    options.addArguments("--headless");
+    displayLog.append("Opening browser...\n");
+    //driver instance created
+    driver = new ChromeDriver(options);
+    
+    //get filter subjects
+    String filter=Config.readFile("sare_hizo.txt");
+     String [] filterArray=filter.split(",");
+     
+     int i=0;
+     while(i<filterArray.length){
+         filterArray[i]=WordUtils.capitalizeFully(filterArray[i]);
+         i++;
+     }
+
+     
+//    if(filter.isEmpty()){
+//      filterArray.toString();
+//
+//    }else{
+//       filterArray=filter.split(",");
+//    }
+    //System.out.println(filterArray[0]+filterArray[1]+filterArray[2]+filterArray[3]+filterArray[4]+filterArray[5]);
+    
+    //get config from file
+    Config conf=new Config();
+    String configSentence=conf.readFile("clink.txt");
+    
+    String configArray[]=configSentence.split(", *");
+    displayLog.append("Loading configuration file...\n");
+    
+     //configs
+     String priceLevel=configArray[0];
+     int delay=Integer.parseInt(configArray[1]);
+     String bidOn=configArray[2];
+   
+    
+     
+     
+     
+     displayLog.append("Signing in...\n");
+     displayLog.append("Authenticating session...\n");
+     
+     //run bot
+       
+       
+       
+       if(Bot.login(driver,email,password)==true){
+           displayLog.append("login successful...\n");
+           displayLog.append("bidding...\n");
+           Bot.bid(driver,delay, priceLevel,bidOn,filterArray, displayLog); 
+           
+       }else{
+       displayLog.append("error, signing in, restart and enter the correct login details...\n");
+       }
+       
+        
+        
+    }
+    
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        try {
+    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+      if ("Nimbus".equals(info.getName())) {
+          javax.swing.UIManager.setLookAndFeel(info.getClassName());
+          break;
+      }
+    }
+  } catch (ClassNotFoundException e) {
+    e.printStackTrace();
+  } catch (InstantiationException e) {
+    e.printStackTrace();
+  } catch (IllegalAccessException e) {
+    e.printStackTrace();
+  } catch (javax.swing.UnsupportedLookAndFeelException e) {
+    e.printStackTrace();
+  } catch (Exception e) {
+    e.printStackTrace();
+  }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new clinkHome(email,code,expiry,password).setVisible(true);
+            }
+        });
+    }
+    
+   
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea displayLog;
+    private javax.swing.JButton home_btnExit;
+    private javax.swing.JButton home_btnLaunch;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane2;
+    // End of variables declaration//GEN-END:variables
+}
